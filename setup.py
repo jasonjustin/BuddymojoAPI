e4b9738862c3a43cb2dd75647a21e7442838b483
jasonjustin <zxc3.zxc46@gmail.com>
2019-11-18 09:09:29 +0800

    Publishing

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="buddymojoAPI",  # Replace with your own username
+    version="1.0.0",
+    author="Jelly Hsu",
+    author_email="zxc3.zxc46@gmail.com",
+    description="An API of cn.buddymojo.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jasonjustin/BuddymojoAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
